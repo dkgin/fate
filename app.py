30cbf4e87f069717fc1a6f40287324bfc0a5160d
Wei-Sheng LU <dkgin307@hotmail.com>
2024-01-23 15:57:22 +0800

    linebotapi

--- app.py
+++ app.py
@@ -15,15 +15,13 @@ excel_data = pd.read_excel(BytesIO(response.content), engine='openpyxl')
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
-
-
 @app.route("/", methods=['POST'])
 def linebot():
     body = request.get_data(as_text=True)                    # 取得收到的訊息內容
     try:
         json_data = json.loads(body)                         # json 格式化訊息內容
+        line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+        handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
         signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
         handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
         msg = json_data['events'][0]['message']['text']      # 取得 LINE 收到的文字訊息
