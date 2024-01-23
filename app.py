6d0481f72525ce3699776a57553ed390ea4946b4
Wei-Sheng LU <dkgin307@hotmail.com>
2024-01-23 15:39:31 +0800

    delete

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ def callback():
     body = request.get_data(as_text=True)                    # 取得收到的訊息內容
     try:
         json_data = json.loads(body)                         # json 格式化訊息內容
-        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
-        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
         signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
         handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
         msg = json_data['events'][0]['message']['text']      # 取得 LINE 收到的文字訊息
